f5032a41f9af6b3e0b6c95bb32372763b4a3c36a
derrickhamilton <derrickhamilton212@gmail.com>
2019-04-30 18:15:33 -0700

    added transparent titles to the card buttons

--- MatchGame/ViewController.swift
+++ MatchGame/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movesMadeLbl: UILabel!
+    @IBOutlet weak var movesLeftLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func flipCard(_ sender: UIButton) {
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
